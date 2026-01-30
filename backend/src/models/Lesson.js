219bd5aa4be542e899253b1af5e73a7b93cefd13
Dagmawi Ephrem <dagmawiephrem44@gmail.com>
2026-01-30 22:07:06 +0300

    corrected typos

--- backend/src/models/Lesson.js
+++ backend/src/models/Lesson.js
@@ -48,21 +48,6 @@ const sectionSchema = new mongoose.Schema({
 }
 });
 
-/**
- * Mongoose schema for a Lesson.
- * 
- * @typedef {Object} Lesson
- * @property {string} topic - The topic of the lesson. Must be unique and is required.
- * @property {string} slug - A unique, required slug identifier for the lesson.
- * @property {Array<Section>} sections - An array of sections associated with the lesson.
- * @property {Date} createdAt - Timestamp for when the lesson was created.
- * @property {Date} updatedAt - Timestamp for when the lesson was last updated.
- *
- * @description
- * This schema defines the structure for Lesson documents in MongoDB, including
- * unique constraints on topic and slug, an embedded array of sections, and automatic
- * management of creation and update timestamps. Virtuals are included in JSON and object outputs.
- */
 const lessonSchema = new mongoose.Schema({
   topic: {
     type: String,
